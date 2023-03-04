089fee09102d52a60b1e81a1c7661a6f393a49ed
Victor Ukoha <victorukay@gmail.com>
2023-03-04 18:22:30 +0100

    crc func completed

--- func.js
+++ func.js
@@ -1,27 +1,85 @@
-const crc = (message, generated) => {
-  let msgArr = message.split("");
-  let genArr = generated.split("");
+const mainFunc = (msgBits, genBits) => {
+  let arrayMsgBits = msgBits.split("");
+  let arrayGenBits = genBits.split("");
 
-  let tBit;
-  let iBit;
-  let counter = 0;
-  while (counter < generated.length - 1) {
-    msgArr.push("0");
-    counter++;
+  const { transmittedBits, remainder } = crcFirstFunc(
+    arrayMsgBits,
+    arrayGenBits
+  );
+  const newMsgBits = [...arrayMsgBits, ...remainder];
+  const { transBits, rem } = crcSecondFunc(newMsgBits, arrayGenBits);
+  // return crcSecondFunc(newMsgBits, genBits);
+  let remSum = rem.split("").reduce((prev, curr) => prev + curr);
+  // console.log(remSum);
+
+  return remSum == 0
+    ? `The message bits: ${msgBits} , transmitted, is error-free.`
+    : `The message bits: ${msgBits}, transmitted, is contains an error.`;
+};
+
+//First CRC Long Division Process
+const crcFirstFunc = (msgBits, genBits) => {
+  let extendedMsgBits = [...msgBits];
+  let extendedMsgBitsChanging = [...msgBits];
+  for (let i = 0; i < genBits.length - 1; i++) {
+    extendedMsgBitsChanging.push(0);
+    extendedMsgBits.push(0);
   }
-  msgArr = msgArr.map((i) => parseInt(i));
-  genArr = genArr.map((i) => parseInt(i));
 
-  let msgInt = parseInt(msgArr.join(""));
-  let genInt = parseInt(generated);
+  let count = 0;
+  let result = [];
+  let transmittedBits = [];
+  let intermediateBit;
+  while (count < msgBits.length) {
+    intermediateBit = extendedMsgBitsChanging[0] & genBits[0];
+    transmittedBits.push(intermediateBit);
 
-  tBit = msgArr[0] & genArr[0];
-  iBit = msgArr[0] & genArr[0];
-  console.log(tBit);
+    for (let i = 0; i < genBits.length; i++) {
+      result.push(extendedMsgBitsChanging[i] ^ (genBits[i] * intermediateBit));
+    }
+
+    result[0] != 0 ? (result = result) : (result = result.splice(1));
+    extendedMsgBitsChanging = [
+      ...result,
+      extendedMsgBits[genBits.length + count],
+    ];
+    result = [];
+    count++;
+  }
+  return {
+    transmittedBits: transmittedBits.join(""),
+    remainder: extendedMsgBitsChanging.join(""),
+  };
 };
 
-const compareFunc = async (a, b) => {
-  return a & b;
+//Second CRC Long Division Process
+const crcSecondFunc = (newMsgBits, genBits) => {
+  let changingMsgBits = newMsgBits;
+  let count = 0;
+  let intermediateBit;
+  let transBits = [];
+  let result = [];
+  let constraint = newMsgBits.length - genBits.length + 1;
+  let sum = changingMsgBits.reduce((prev, curr) => prev + curr);
+  while (count < constraint && sum != 0) {
+    intermediateBit = changingMsgBits[0] & genBits[0];
+    transBits.push(intermediateBit);
+    for (let i = 0; i < genBits.length; i++) {
+      result.push(changingMsgBits[i] ^ (genBits[i] * intermediateBit));
+    }
+    result[0] == 0 ? (result = result.splice(1)) : (result = result);
+    changingMsgBits = [...result, newMsgBits[genBits.length + count]];
+    result = [];
+    count++;
+  }
+  changingMsgBits = changingMsgBits.map((el) => {
+    return el == null ? (el = 0) : (el = el);
+  });
+  return {
+    transBits: transBits.join(""),
+    rem: changingMsgBits.splice(0, changingMsgBits.length).join(""),
+  };
 };
 
-crc("1011", "1001");
+console.log(mainFunc("1010001101", "110101"));
+console.log(mainFunc("1011", "1001"));
